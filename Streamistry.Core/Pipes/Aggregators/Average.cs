d6949057fd9e238007be75da241c17e083c7a816
Cédric L. Charlier <seddryck@gmail.com>
2024-08-19 22:55:04 +0200

    feat: add aggregator to accumulates a stateful function across the elements

---
+++ Streamistry.Core/Pipes/Aggregators/Average.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Streamistry.Pipes.Aggregators;
+
+public struct AverageState<T>(T count, T total) where T : INumber<T>
+{
+    public AverageState<T> Append(T? value)
+    {
+        if (value is not null)
+        {
+            count += T.One;
+            total += value;
+        }
+        return this;
+    }
+
+    public readonly T? Select()
+        => count > T.Zero ? total / count : default;
+
+    public static readonly AverageState<T> @Default = new();
+}
+
+public class Average<T> : Aggregator<T, AverageState<T>, T> where T : INumber<T>
+{
+    public Average(IChainablePipe<T> upstream)
+        : base(upstream
+            , (x, y) => x.Append(y)
+            , (x) => x.Select()
+            , AverageState<T>.Default)
+    { }
+}
+
+public class Average<T, U> : Aggregator<T, AverageState<U>, U>
+        where T : INumber<T>
+        where U : INumber<U>
+{
+    public Average(IChainablePipe<T> upstream)
+        : base(upstream
+            , (x, y) => x.Append(y is null ? default : U.CreateChecked(y))
+            , (x) => x.Select()
+            , AverageState<U>.Default)
+    { }
+}
---
+++ Streamistry.Core/Pipes/Aggregators/Count.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Streamistry.Pipes.Aggregators;
+public class Count<T> : Aggregator<T, int, int>
+{
+    public Count(IChainablePipe<T> upstream)
+        : base(upstream
+            , (x, y) => y is null ? x : ++x, (x) => x)
+    { }
+}
+
+public class Count<T, U> : Aggregator<T, U, U>
+    where U : INumber<U>
+{
+    public Count(IChainablePipe<T> upstream)
+        : base(upstream
+            , (x, y) => y is null ? x : (x is null ? U.One : ++x), (x) => x)
+    { }
+}
