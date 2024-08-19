a3a229abe7b61526afa77294982ece328ddd204c
Cédric L. Charlier <seddryck@gmail.com>
2024-08-20 01:54:07 +0200

    feat: add a mapper to retrieve the value of a property of an object

---
+++ Streamistry.Core/Pipes/Mappers/Plucker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamistry.Pipes.Mappers;
+public class Plucker<TInput, TOutput> : Mapper<TInput, TOutput>
+{
+    public Plucker(IChainablePipe<TInput> upstream, Expression<Func<TInput, TOutput?>> expr)
+        : base(upstream, x => RetrieveProperty(x, expr))
+    { }
+
+    private static TOutput? RetrieveProperty(TInput? input, Expression<Func<TInput, TOutput?>> lambda)
+    {
+        var propInfo = GetPropertyInfo(lambda);
+        return (TOutput?)propInfo.GetValue(input, null);
+    }
+
+    private static PropertyInfo GetPropertyInfo(Expression<Func<TInput, TOutput?>> lambda)
+    {
+        if (lambda.Body is not MemberExpression member)
+            throw new ArgumentException($"Expression '{lambda}' refers to a method, not a property.");
+
+        if (member.Member is not PropertyInfo propInfo)
+            throw new ArgumentException($"Expression '{lambda}' refers to a field, not a property.");
+
+        var type = typeof(TInput);
+        if (propInfo.ReflectedType != null && type != propInfo.ReflectedType && !type.IsSubclassOf(propInfo.ReflectedType))
+            throw new ArgumentException($"Expression '{lambda}' refers to a property that is not a property from type {type}.");
+
+        return propInfo;
+    }
+}
---
+++ Streamistry.Testing/Pipes/Mappers/PluckerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Streamistry.Pipes.Mappers;
+using Streamistry.Pipes.Sinks;
+
+namespace Streamistry.Testing.Pipes.Mappers;
+public class PluckerTests
+{
+    private record class Human(string Name, DateOnly BirthDay)
+    { }
+
+    [Test]
+    public void Emit_HumanPluckerOnBirthDay_BirthDay()
+    {
+        var pipeline = new Pipeline<Human>();
+        var plucker = new Plucker<Human, DateOnly>(pipeline, h => h.BirthDay);
+        var sink = new MemorySink<DateOnly>(plucker);
+
+        pipeline.Emit(new Human("Albert Einstein", new DateOnly(1879, 3, 14)));
+        Assert.That(sink.State.Last(), Is.EqualTo(new DateOnly(1879, 3, 14)));
+    }
+}
