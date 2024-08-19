12f97aa911efca18109909b59284ef84af2356be
Cédric L. Charlier <seddryck@gmail.com>
2024-08-19 20:55:57 +0200

    feat: add mapper to apply a specified function to each element

---
+++ Streamistry.Core/Mapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamistry;
+
+/// <summary>
+/// Represents a pipeline element that applies a specified function to each element within a batch of this stream.
+/// The output stream is determined by the result of the function applied to the input elements.
+/// </summary>
+/// <typeparam name="TInput">The type of the elements in the input stream.</typeparam>
+/// <typeparam name="TOutput">The type of the elements in the output stream after the function is applied.</typeparam>
+public class Mapper<TInput, TOutput> : IProcessablePipe<TInput>, IChainablePipe<TOutput>
+{
+    private ICollection<IProcessablePipe<TOutput>> Downstreams { get; } = [];
+    public Func<TInput?, TOutput?> Function { get; init; }
+
+    public Mapper(IChainablePipe<TInput> upstream, Func<TInput?, TOutput?> function)
+    {
+        upstream.RegisterDownstream(this);
+        Function = function;
+    }
+
+    public void RegisterDownstream(IProcessablePipe<TOutput> element)
+        => Downstreams.Add(element);
+
+    public void Emit(TInput? obj)
+    {
+        var result = Function.Invoke(obj);
+        foreach (var downstream in Downstreams)
+            downstream.Emit(result);
+    }
+}
