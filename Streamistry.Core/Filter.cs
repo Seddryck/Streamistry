8bcbd7a5ab6915395d0002c24f2f7f0ed9bda23a
Cédric L. Charlier <seddryck@gmail.com>
2024-08-21 21:52:49 +0200

    feat: add filter to decide which batch elements are pushed downstream (#15)

---
+++ Streamistry.Core/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamistry;
+
+/// <summary>
+/// Represents a pipeline element that applies a predicate function to each element within a batch of this stream.
+/// The output stream is composed of elements that satisfy the predicate; elements that do not satisfy the predicate are excluded from the downstream stream.
+/// </summary>
+/// <typeparam name="TInput">The type of the elements in both the input and output streams.</typeparam>
+public class Filter<TInput> : ChainablePipe<TInput>, IProcessablePipe<TInput>
+{
+    public Func<TInput?, bool> Predicate { get; init; }
+
+    public Filter(IChainablePipe<TInput> upstream, Func<TInput?, bool> predicate)
+    {
+        upstream.RegisterDownstream(Emit);
+        Predicate = predicate;
+    }
+
+    public void Emit(TInput? obj)
+    {
+        if (Predicate.Invoke(obj))
+            PushDownstream(obj);
+    }
+}
---
+++ Streamistry.Testing/FilterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Streamistry.Pipes.Sinks;
+
+namespace Streamistry.Testing;
+public class FilterTests
+{
+    [Test]
+    public void Emit_InlinePositive_Successful()
+    {
+        var pipeline = new Pipeline<int>();
+        var filter = new Filter<int>(pipeline, x => x >= 0);
+        var sink = new MemorySink<int>(filter);
+        filter.Emit(11);
+
+        Assert.That(sink.State, Has.Count.EqualTo(1));
+        Assert.That(sink.State.First(), Is.EqualTo(11));
+    }
+
+    [Test]
+    public void Emit_InlinePositive_DontEmit()
+    {
+        var pipeline = new Pipeline<int>();
+        var filter = new Filter<int>(pipeline, x => x >= 0);
+        var sink = new MemorySink<int>(filter);
+        filter.Emit(-11);
+
+        Assert.That(sink.State, Has.Count.EqualTo(0));
+    }
+}
