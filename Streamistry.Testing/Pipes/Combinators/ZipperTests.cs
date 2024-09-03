1709ef3f140960ffb901784e1e9dd3f9e06ad09c
Cédric L. Charlier <seddryck@gmail.com>
2024-09-03 08:24:40 +0200

    fix: when both input ports have same type confusion of queues

--- Streamistry.Testing/Pipes/Combinators/ZipperTests.cs
+++ Streamistry.Testing/Pipes/Combinators/ZipperTests.cs
@@ -26,6 +26,24 @@ public class ZipperTests
     }
 
     [Test]
+    public void Emit_ZipSameType_Successful()
+    {
+        var first = new Pipeline<int>();
+        var second = new Pipeline<int>();
+        var combinator = new Zipper<int, int, int>(first, second, (x, y) => x*y);
+        var sink = new MemorySink<int>(combinator);
+
+        second.Emit(5);
+        Assert.That(sink.State, Has.Count.EqualTo(0));
+        second.Emit(8);
+        Assert.That(sink.State, Has.Count.EqualTo(0));
+
+        first.Emit(10);
+        Assert.That(sink.State, Has.Count.EqualTo(1));
+        Assert.That(sink.State.Last(), Is.EqualTo(50));
+    }
+
+    [Test]
     public void Emit_ZipWhenBothEmittedReversed_Successful()
     {
         var first = new Pipeline<char>();
