5574d310a60292ed7dc5639291ee341fb107f812
Cédric L. Charlier <seddryck@gmail.com>
2024-09-03 23:44:53 +0200

    refactor: remove some unneeded dependencies and usings

--- Streamistry.Core/Pipeline.cs
+++ Streamistry.Core/Pipeline.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Mono.Cecil;
 using Streamistry.Observability;
 
 namespace Streamistry;
--- Streamistry.Testing/UnionTests.cs
+++ Streamistry.Testing/UnionTests.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using Mono.Cecil;
 using NUnit.Framework;
 using Streamistry.Pipes.Sources;
 using Streamistry.Testability;
